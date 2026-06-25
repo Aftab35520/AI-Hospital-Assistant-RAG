920fb70871b055569ae8293dc622fdeda0edbf3b
aftab35520 <aftab.alam35520@gmail.com>
2026-06-25 20:53:12 +0530

    Initial commit

--- db/connection.py
+++ db/connection.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 import json
-uri = "mongodb://localhost:27017"
+uri = "mongodb+srv://Aftab355201:Aftab355201@cluster0.cn5rpym.mongodb.net/?appName=Cluster0"
 client = MongoClient(uri)
 database = client.get_database("Hospital_Langgraph")
 Appointment = database.get_collection("Appointment")
