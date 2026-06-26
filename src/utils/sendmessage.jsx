0e4931bb84cd9d91b6583908302da7ad0da6a319
aftab35520 <aftab.alam35520@gmail.com>
2026-06-26 16:57:07 +0530

    endpoint

--- src/utils/sendmessage.jsx
+++ src/utils/sendmessage.jsx
@@ -1,14 +1,20 @@
-
-
 export default async function sendmessage(message) {
-    try{
-        let response=await fetch(`https://ai-hospital-assistant-rag-1.onrender.com/${message}/${"usderd1ddd0"}`)
-        response=await response.json()
-        return(response)
-    }
-    catch(e){
-        console.log(e)
-        return "e.message"
-    }
-   
+  let userId = localStorage.getItem("userId");
+
+  if (!userId) {
+    userId = crypto.randomUUID(); 
+    localStorage.setItem("userId", userId);
+  }
+
+  try {
+    const response = await fetch(
+      `https://ai-hospital-assistant-rag-1.onrender.com/${encodeURIComponent(message)}/${userId}`
+    );
+
+    const data = await response.json();
+    return data;
+  } catch (e) {
+    console.error(e);
+    return e.message;
+  }
 }
