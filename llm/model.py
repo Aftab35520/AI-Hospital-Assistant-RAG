56badc077ec5dacacad4ceca069c92418aab84bf
aftab35520 <aftab.alam35520@gmail.com>
2026-06-30 17:17:22 +0530

    j

--- llm/model.py
+++ llm/model.py
@@ -2,10 +2,14 @@ from langchain_groq import ChatGroq
 import os
 from dotenv import load_dotenv
 load_dotenv()
-Chat_Model=ChatGroq(
-    groq_api_key=os.getenv("ApiKey"),
-    model="llama-3.3-70b-versatile"
+
+from langchain_openai import AzureChatOpenAI
+
+Chat_Model = AzureChatOpenAI(
+    azure_endpoint=os.getenv("AZURE_ENDPOINT"),
+    api_key=os.getenv("AZURE_API_KEY"),
+    azure_deployment=os.getenv("AZURE_DEPLOYMENT"),
+    api_version="2024-10-21",
+    streaming=True,
+    temperature=0
 )
-
-
-
