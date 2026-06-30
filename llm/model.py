d3a7b9ae38d3242e8c3b42b490df252e3248bb98
aftab35520 <aftab.alam35520@gmail.com>
2026-06-30 17:28:12 +0530

    j

--- llm/model.py
+++ llm/model.py
@@ -10,6 +10,6 @@ Chat_Model = AzureChatOpenAI(
     api_key=os.getenv("AZURE_API_KEY"),
     azure_deployment=os.getenv("AZURE_DEPLOYMENT"),
     api_version="2024-10-21",
-    streaming=True,
+  
     temperature=0
 )
